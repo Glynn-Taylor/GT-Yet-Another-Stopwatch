66fe72d3381a155c77a27a2d85d9d30576e4bf5b
Glynn Taylor <glynntaylorwork@hotmail.co.uk>
2017-09-25 12:57:48 +0100

    Created a simple start/pause, reset stopwatch, always on top and black borderless background

---
+++ GTStopwatch/StopwatchForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GTStopwatch
+{
+    partial class StopwatchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startStopBtn = new System.Windows.Forms.Button();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.timeDisplayLbl = new System.Windows.Forms.Label();
+            this.quitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startStopBtn
+            // 
+            this.startStopBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.startStopBtn.Location = new System.Drawing.Point(83, 56);
+            this.startStopBtn.Name = "startStopBtn";
+            this.startStopBtn.Size = new System.Drawing.Size(76, 23);
+            this.startStopBtn.TabIndex = 0;
+            this.startStopBtn.Text = "Start";
+            this.startStopBtn.UseVisualStyleBackColor = false;
+            this.startStopBtn.Click += new System.EventHandler(this.StartStopClick);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.resetBtn.Location = new System.Drawing.Point(12, 56);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(65, 23);
+            this.resetBtn.TabIndex = 1;
+            this.resetBtn.Text = "Reset";
+            this.resetBtn.UseVisualStyleBackColor = false;
+            this.resetBtn.Click += new System.EventHandler(this.OnResetClick);
+            // 
+            // timeDisplayLbl
+            // 
+            this.timeDisplayLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeDisplayLbl.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.timeDisplayLbl.Location = new System.Drawing.Point(12, 9);
+            this.timeDisplayLbl.Name = "timeDisplayLbl";
+            this.timeDisplayLbl.Size = new System.Drawing.Size(171, 44);
+            this.timeDisplayLbl.TabIndex = 3;
+            this.timeDisplayLbl.Text = "00:00:00";
+            this.timeDisplayLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // quitBtn
+            // 
+            this.quitBtn.ForeColor = System.Drawing.SystemColors.Control;
+            this.quitBtn.Location = new System.Drawing.Point(165, 56);
+            this.quitBtn.Name = "quitBtn";
+            this.quitBtn.Size = new System.Drawing.Size(18, 23);
+            this.quitBtn.TabIndex = 4;
+            this.quitBtn.Text = "X";
+            this.quitBtn.UseVisualStyleBackColor = false;
+            this.quitBtn.Click += new System.EventHandler(this.QuitClick);
+            // 
+            // StopwatchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(195, 81);
+            this.ControlBox = false;
+            this.Controls.Add(this.quitBtn);
+            this.Controls.Add(this.timeDisplayLbl);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.startStopBtn);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StopwatchForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "GT Stopwatch";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startStopBtn;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Label timeDisplayLbl;
+        private System.Windows.Forms.Button quitBtn;
+    }
+}
+
---
+++ GTStopwatch/StopwatchForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTStopwatch
+{
+    // ---[[[ GT STOPWATCH ]]]---
+    // A winform for a basic timer, many thanks to Mark Ashley Bell for
+    // a quickstart version of the code, I have modified it a bit to add in
+    // drag handling, pause/play and removed the total time timer
+    // Ref: https://markb.co.uk/creating-a-simple-stopwatchtimer-application-with-c-windows-forms.html
+    // Created by Glynn Taylor for use in timelapse recording
+    // http://www.glynntaylor.co.uk
+    //--------------------------------------------------------
+    public partial class StopwatchForm : Form
+    {
+        #region ---[[[ DRAG HANDLING VARIABLES / REFERENCES ]]]---
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+
+        #region ---[[[ PRIVATE VARIABLES ]]]---
+        private Timer timer;
+        private DateTime startTime = DateTime.MinValue;
+        private TimeSpan previousElapsedTime = TimeSpan.Zero;
+        private bool timerRunning = false;
+        #endregion
+
+        #region ---[[[ START AND UPDATE ]]]---
+        public StopwatchForm()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(_timer_Tick);
+            this.MouseDown += FormMouseDown;
+        }
+
+        void _timer_Tick(object sender, EventArgs e)
+        {
+            var timeSinceStartTime = DateTime.Now - startTime;
+            timeSinceStartTime = new TimeSpan(timeSinceStartTime.Hours,
+                                              timeSinceStartTime.Minutes,
+                                              timeSinceStartTime.Seconds);
+            timeDisplayLbl.Text = (timeSinceStartTime + previousElapsedTime).ToString();
+        }
+        #endregion
+
+        #region ---[[[ BUTTONS ]]]---
+        private void StartStopClick(object sender, EventArgs e)
+        {
+            if (!timerRunning)
+            {
+                StartTimer();
+            }
+            else
+            {
+                PauseTimer();
+            }
+        }
+
+        private void OnResetClick(object sender, EventArgs e)
+        {
+            StopTimer();
+        }
+
+        private void QuitClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // FORM DRAG HANDLING FOR REPOSITIONING
+        private void FormMouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        #region ---[[[ TIMER FUNCTIONS ]]]---
+        public void StartTimer()
+        {
+            startTime = DateTime.Now;
+            timer.Start();
+            timerRunning = true;
+            startStopBtn.Text = "Pause";
+        }
+
+        public void PauseTimer()
+        {
+            timer.Stop();
+            timerRunning = false;
+            previousElapsedTime = previousElapsedTime + (DateTime.Now - startTime);
+            startStopBtn.Text = "Start";
+        }
+
+        public void StopTimer()
+        {
+            PauseTimer();
+            timeDisplayLbl.Text = "00:00:00";
+            previousElapsedTime = TimeSpan.Zero;
+        }
+        #endregion
+    }
+}
